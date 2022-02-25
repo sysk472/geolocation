4e5ea263cd04e6f3d6a797dd207a9af4738fa0d5
Wiewior <sysk472@gmail.com>
2022-02-25 18:57:36 +0100

    Generated Controller, added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, constraints: { format: 'json' } do
+    namespace :v1 do
+      resources :geological_locations, only: %i[show create destroy]
+    end
+  end
 end
