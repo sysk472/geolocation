24b843caad65ae9bed1f294c629e8f1a07daa24a
Wiewior <sysk472@gmail.com>
2022-12-15 20:20:56 +0100

    Add comment 2

--- spec/services/geolocations/presenter_spec.rb
+++ spec/services/geolocations/presenter_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe Geolocations::Presenter, type: :transaction do
       #check it
       #che
       #check
+      
       allow_any_instance_of(ExternalApiCaller).to receive(:call).with('google.com').and_return(Success(build(:geolocation, :google).data))
       is_expected.to be_success
     end
